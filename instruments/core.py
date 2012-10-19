9d2cbd12070f8822f5a46657a403c203987b4a0a
MalphasWats <malphas@subdimension.co.uk>
2012-10-19 14:52:12 +0100

    moved load_blueprints() into __init__ to keep core to views

--- instruments/core.py
+++ instruments/core.py
@@ -2,12 +2,8 @@ from flask import (Flask, request, session, g, redirect, url_for,
                    abort, render_template, flash, jsonify)
 
 import hashlib
-import os
-import imp
 
 import database
-
-#from functools import wraps
 
 SECRET_KEY = 'banana'
 
@@ -122,47 +118,6 @@ def update_password():
 @public_endpoint
 def test_route():
     print "test reached"
-    
-    
-def load_blueprints():
-    """ 
-    Called in __init__.py on first startup. 
-
-    Looks for blueprints, loads them and generates
-    a blueprints global for jinja
-    """
-    
-    app.jinja_env.globals['blueprints'] = []
-    
-    path = 'blueprints'
-    dir_list = os.listdir(path)
-    blueprints = {}
-    
-    for fname in dir_list:
-        if os.path.isdir(os.path.join(path, fname)) and os.path.exists(os.path.join(path, fname, '__init__.py')):
-            f, filename, descr = imp.find_module(fname, [path])
-            blueprints[fname] = imp.load_module(fname, f, filename, descr)
-            
-        elif os.path.isfile(os.path.join(path, fname)):
-            name, ext = os.path.splitext(fname)
-            if ext == '.py' and not name == '__init__':
-                f, filename, descr = imp.find_module(name, [path])
-                blueprints[fname] = imp.load_module(name, f, filename, descr)
-
-    for blueprint in blueprints.values():
-        
-        blueprint_name = getattr(blueprint, '__name__')
-        blueprint = getattr(blueprint, blueprint_name, blueprint)
-        
-        blueprint_label = getattr(blueprint, 'LABEL', None)
-        blueprint_icon = getattr(blueprint, 'ICON', 'link')
-                
-        app.register_blueprint(getattr(blueprint, 'blueprint'), url_prefix='/%s' % blueprint_name)
-        if blueprint_label:
-            app.jinja_env.globals['blueprints'].append( ("%s.index"%blueprint_name, blueprint_label, blueprint_icon) )
-        
-    app.config['registered_blueprints'] = blueprints
-    
     
     
 @app.errorhandler(404)

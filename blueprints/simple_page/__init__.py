f4c30e222002fb82aa01f80e0252cf2e428ac838
MalphasWats <malphas@subdimension.co.uk>
2012-11-02 12:01:39 +0000

    reworked simple_page example blueprint

--- blueprints/simple_page/__init__.py
+++ blueprints/simple_page/__init__.py
@@ -1,1 +1,14 @@
-from simple_page import blueprint
+from flask import Blueprint, render_template
+
+blueprint = Blueprint('simple_page', __name__,
+                        template_folder='templates')
+                        
+                        
+LABEL = 'Simple Page'
+ADMIN_LABEL = 'Simple Page Administration'
+ICON = 'globe'
+
+import simple_page.core
+
+def get_admin_panel():
+    return render_template('widgets/admin_panel.html')
